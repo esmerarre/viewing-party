d673a8d140dadddbe19c6a01392b48bea847a9bc
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2025-09-22 11:19:16 -0700

    updated user data in add_to_watched

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -13,6 +13,14 @@ def create_movie(title, genre, rating):
     
     return movie
 
+def add_to_watched(user_data, movie):
+
+    user_data["watched"].append(movie)
+    
+
+    return user_data
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
