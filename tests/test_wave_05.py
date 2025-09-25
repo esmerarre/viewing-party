e15be8198e5069eedf26ea2da51527e7c87f273c
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2025-09-25 07:57:51 -0700

    format tests wave_5

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -55,6 +55,8 @@ def test_new_genre_rec_from_empty_friends():
 
     # Act
     recommendations = get_new_rec_by_genre(sonyas_data)
+    
+    #Assert
     assert len(recommendations) == 0
 
 #@pytest.mark.skip()
